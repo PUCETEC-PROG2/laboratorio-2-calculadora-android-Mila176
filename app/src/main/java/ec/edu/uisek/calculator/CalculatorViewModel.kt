5cb92a3f7bee9fdd8674e22d112788bf2ab7d892
Camila Sandoval <camisandoval2005@gmail.com>
2025-10-28 15:38:39 -0500

    Conexión de un Model y Composable

--- app/src/main/java/ec/edu/uisek/calculator/CalculatorViewModel.kt
+++ app/src/main/java/ec/edu/uisek/calculator/CalculatorViewModel.kt
@@ -38,15 +38,57 @@ class CalculatorViewModel : ViewModel (){
     }
 
     private fun performCalculation() {
-        TODO("Not yet implemented")
+        val num1 = number1.toDoubleOrNull()
+        val num2 = number2.toDoubleOrNull()
+        if(num1 != null && num2 != null&& operator !=null){
+            val result = when(operator) {
+                "+" -> num1 + num2
+                "-" -> num1 - num2
+                "×" -> num1 * num2
+                "÷" -> if(num2 != 0.0) num1 / num2 else Double.NaN
+                else -> 0.0
+            }
+            clearAll()
+            val resultString = if (result.isNaN()) "Error" else result.toString().removeSuffix(".0")
+            number1 = if (result.isNaN()) "" else resultString
+            state = state.copy(resultString)
+        }
     }
 
     private fun clearAll() {
-        TODO("Not yet implemented")
+        number1 = ""
+        number2 = ""
+        operator = null
+        state = state.copy(display = "0")
     }
 
     private fun clearLast() {
-        TODO("Not yet implemented")
+        if(operator == null) {
+            if(number1.isNotBlank()){
+                number1 = number1.dropLast(1)
+                state=state.copy(
+                if(number1.isBlank())
+                    "0"
+                else
+                    number1
+                )
+            }
+        } else {
+            if(number2.isNotBlank()) {
+                if (number2.isNotBlank()) {
+                    number2 = number2.dropLast(1)
+                    state = state.copy(
+                        if (number2.isBlank())
+                            "0"
+                        else
+                            number2
+                    )
+                } else {
+                    operator = null
+                    state = state.copy(number1)
+                }
+            }
+        }
     }
 
     private fun enterDecimal() {
